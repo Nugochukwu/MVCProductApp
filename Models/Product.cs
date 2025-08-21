11eaaa6f0b4b642b69eaed1ec86e8bdcbc61206a
N.Ugochukwu <115807244+Nugochukwu@users.noreply.github.com>
2025-08-21 12:32:30 +0100

    Simple Input Validation

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,9 +1,13 @@
-﻿namespace MVCProductApp.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace MVCProductApp.Models
 {
     public class Product
     {
         public int ID { get; set; }
-        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = "";
         public decimal Price { get; set; }
         public string Description { get; set; }
     }
